ebfb157fc1ddf3bb5aad53d494ba3fc9ab7c5253
carterdooley <carter.j.dooley43@gmail.com>
2022-07-18 14:51:04 -0500

    Updated Rest File

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,7 +9,7 @@ public class Main {
         bank.addBranch("Section 705", 70503);
         Branch thisBranch = bank.getBranch("Section 705");
         thisBranch.addUser("Carter", 500.00, "password");
-        thisBranch.addUser("Carter", 500.00, "password");
+//        thisBranch.addUser("Carter", 500.00, "password");
         User Carter = thisBranch.getUser("Carter");
         Carter.depositFunds(1.00);
         Carter.withdrawFunds(2.00);
