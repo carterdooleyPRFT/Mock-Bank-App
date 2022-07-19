33e42360a8e980a21f431b3525e6ea6726129823
carterdooley <carter.j.dooley43@gmail.com>
2022-07-19 13:05:42 -0500

    Gui Additions

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,49 +1,9 @@
-import Bank.Bank;
-import Branch.Branch;
-import User.User;
-
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import GUI.HomePage;
 
 public class Main {
 
     public static void main(String[] args) {
+        new HomePage();
 
-        final JFrame bankApp = new JFrame("Banking");
-
-
-
-
-//        Create Account Button Control
-        JButton createAccount = new JButton("Create Account");
-        createAccount.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                bankApp.dispose();
-                new LoginPage();
-            }
-        });
-
-
-
-
-//        Login Button Control
-        JButton login = new JButton("Login");
-
-
-
-
-
-        bankApp.setSize(350, 250);
-        bankApp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        bankApp.setResizable(true);
-        bankApp.add(createAccount);
-        bankApp.add(login);
-        bankApp.setLayout(new FlowLayout());
     }
-
-
-
 }
